8508faaca3146d006f83c60874442e144cafbf57
서윤혁 <yunhyeok@seoyunhyeog-ui-MacBookAir.local>
2022-05-13 22:42:11 +0900

    add user login service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {}
