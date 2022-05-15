6e29a06b42083b392e55bc2e974ad0f3ff6b5997
syh <dbsgur6896@gmail.com>
2022-05-15 21:14:12 +0900

    add user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,36 +1,11 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserLoginDto } from './dto/user-login.dto';
-import { VerifyEmailDto } from './dto/verify-email.dto';
-import { UserInfo } from './UserInfo';
+import { Module } from '@nestjs/common';
+import { EmailModule } from 'src/email/email.module';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {
-  constructor(private usersService: UsersService) { }
-
-  @Post()
-  async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    const { name, email, password } = dto;
-    await this.usersService.createUser(name, email, password);
-  }
-
-  @Post('/email-verify')
-  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
-    const { signupVerifyToken } = dto;
-
-    return await this.usersService.verifyEmail(signupVerifyToken);
-  }
-
-  @Post('/login')
-  async login(@Body() dto: UserLoginDto): Promise<string> {
-    const { email, password } = dto;
-
-    return await this.usersService.login(email, password);
-  }
-
-  @Get('/:id')
-  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
-    return await this.usersService.getUserInfo(userId);
-  }
-}
+@Module({
+  imports: [EmailModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
